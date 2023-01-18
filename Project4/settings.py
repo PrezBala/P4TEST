823455880ef5e58134052a7c244e9c09695a7cc8
PrezBala <prez5aw@live.com>
2023-01-18 12:34:16 +0000

    blabla

--- Project4/settings.py
+++ Project4/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zcm6ahghxwlz95%iu!-_m=m!ym9t1b$5l$^38%6wfaoxq9pm*-'
+SECRET_KEY = 'django-insecure-@22z%4y$4&p@!ji0ts*cc!qm=2v_vugc8ohbijk&r67gm*9o_u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

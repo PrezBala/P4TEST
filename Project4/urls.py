aaf4b5cc9031a61128f88effd87913cebe7bc58d
PrezBala <prez5aw@live.com>
2023-01-29 14:51:35 +0000

    chaos

--- Project4/urls.py
+++ Project4/urls.py
@@ -5,6 +5,8 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls), 
-    path('', include("main.urls")), 
+    path('admin/', admin.site.urls),
+    path('', include("main.urls")),
+    path('tinymce/', include('tinymce.urls')),
+    path('hitcount/', include('hitcount.urls', namespace='hitcount')),
 	]
--- main/models.py
+++ main/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth import get_user_model
 from django_resized import ResizedImageField
+from tinymce.models import HTMLField
+from hitcount.models import HitCountMixin, HitCount
+from django.contrib.contenttypes.fields import GenericRelation
 
 User = get_user_model()
 
@@ -10,7 +13,7 @@ class Author(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     fullname = models.CharField(max_length=40, blank=True)
     slug = slug = models.SlugField(max_length=400, unique=True, blank=True)
-    bio = models.TextField()
+    bio = HTMLField()
     points = models.IntegerField(default=0)
     profile_pic = ResizedImageField(
         size=[50, 80],
@@ -21,16 +24,42 @@ class Author(models.Model):
         blank=True
     )
 
+    def __str__(self):
+        return self.fullname
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.fullname)
         super(Post, self).save(*args, **kwargs)
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=400, unique=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=400)
     slug = models.SlugField(max_length=400, unique=True, blank=True)
     user = models.ForeignKey(Author, on_delete=models.CASCADE)
+    content = HTMLField()
+    categories = models.ManyToManyField(Category)
+    date = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+    hit_count_generic = GenericRelation(HitCount, object_id_field='object_pk',
+        related_query_name='hit_count_generic_relation'
+    )
 
     def save(self, *args, **kwargs):
         if not self.slug:
